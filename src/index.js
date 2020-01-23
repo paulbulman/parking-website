a780eec759afded54a881e56e6817af9b98bd5f6
Paul Bulman <paulbulman@gmail.com>
2020-01-23 21:03:09 +0000

    Add Amplify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from "aws-amplify";
+
+if (process.env.NODE_ENV === "production") {
+  Amplify.configure({
+    Auth: {
+      region: "eu-west-2",
+      userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+      userPoolWebClientId: process.env.REACT_APP_COGNITO_APP_CLIENT_ID
+    }
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

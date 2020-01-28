27aed3fd6a89825c8c90ca1262af4b8e70b2e110
Paul Bulman <paulbulman@gmail.com>
2020-01-28 08:54:52 +0000

    Remove unnecessary default dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import Amplify from "@aws-amplify/core";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -16,8 +15,3 @@ if (process.env.NODE_ENV === "production") {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

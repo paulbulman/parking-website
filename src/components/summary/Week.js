2d5f7151911522ff080ccb95cfffd07fb7b313ea
Paul Bulman <paulbulman@gmail.com>
2020-02-18 16:17:44 +0000

    Refactor

--- src/components/summary/Week.js
+++ src/components/summary/Week.js
@@ -4,17 +4,21 @@ import moment from "moment";
 import Day from "./Day";
 
 const Week = ({ data }) => {
-  const createDayCell = (data, date) => {
-    const dayData = data.find(d => d.date.isSame(date));
+  const createDayCell = date => {
+    const singleDayData = data.find(d => d.date.isSame(date));
 
-    return <td key={date}>{dayData ? <Day data={dayData} /> : null}</td>;
+    return (
+      <td key={date}>
+        {singleDayData ? <Day data={singleDayData} /> : <>&nbsp;</>}
+      </td>
+    );
   };
 
   const monday = moment(data[0].date).day("monday");
 
-  return [...Array(5).keys()].map(k =>
-    createDayCell(data, moment(monday).add(k, "day"))
-  );
+  const days = [...Array(5).keys()].map(k => moment(monday).add(k, "day"));
+
+  return days.map(d => createDayCell(d));
 };
 
 export default Week;

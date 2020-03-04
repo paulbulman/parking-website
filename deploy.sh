d69f4c917f6b34e795fba1a6a88ea9e801576839
Paul Bulman <paulbulman@gmail.com>
2020-03-04 19:25:32 +0000

    Add no-cache header to index.html on deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ then
   npm i --production
   npm run build
   aws s3 cp build s3://$BUCKET_NAME --recursive
+  aws s3 cp build/index.html s3://$BUCKET_NAME/index.html --metadata-directive REPLACE --cache-control no-cache
   aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
 fi

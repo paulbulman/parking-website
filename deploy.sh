56d75c9170a512dd67ee095f6115d02b65d3b67a
Paul Bulman <paulbulman@gmail.com>
2020-03-08 10:25:45 +0000

    Remove old version on deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ then
   rm -rf node_modules
   npm i --production
   npm run build
-  aws s3 cp build s3://$BUCKET_NAME --recursive
+  aws s3 sync build s3://$BUCKET_NAME --delete
   aws s3 cp build/index.html s3://$BUCKET_NAME/index.html --metadata-directive REPLACE --cache-control no-cache
   aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
 fi

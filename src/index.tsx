e32b3d38ec7128f724875bbd74085ec45ac46490
Paul Bulman <paulbulman@gmail.com>
2021-06-22 17:54:42 +0100

    Remove web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { ToastContainer, Zoom } from "react-toastify";
 import { AuthContextProvider } from "./context/auth";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -37,8 +36,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

709fcc1a76cad43e1a1f7393bb242feadc6f5211
Paul Bulman <paulbulman@gmail.com>
2021-10-15 18:03:56 +0100

    #70 stack footer elements below mobile breakpoint

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
 
+const Licence = () => (
+  <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">
+    GNU General Public License v3
+  </a>
+);
+
+const Privacy = () => <Link to="/privacy">Privacy Policy</Link>;
+
 export const Footer = () => {
   return (
     <footer className="is-flex is-justify-content-center pb-2">
-      <div>
-        <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">
-          GNU General Public License v3
-        </a>
+      <div className="is-hidden-mobile">
+        <Licence />
         {" | "}
-        <Link to="/privacy">Privacy Policy</Link>
+        <Privacy />
+      </div>
+      <div className="is-hidden-tablet has-text-centered">
+        <div className="pb-1">
+          <Licence />
+        </div>
+        <div>
+          <Privacy />
+        </div>
       </div>
     </footer>
   );

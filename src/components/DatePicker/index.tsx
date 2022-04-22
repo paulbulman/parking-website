eaf0a40623b368e7cc34546276641dcbba8c05af
Paul Bulman <paulbulman@gmail.com>
2022-04-22 17:26:16 +0100

    Use new react-day-picker interface

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,48 +1,69 @@
-import { DateUtils } from "react-day-picker";
-import DayPickerInput from "react-day-picker/DayPickerInput";
+import { useEffect, useState, useRef } from "react";
+import { findDOMNode } from "react-dom";
+import { DayPicker } from "react-day-picker";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
-import { format, parse } from "date-fns";
+import { format } from "date-fns";
 import { DatePickerProps } from "./types";
-
-const formatDate = (localDate: Date, formatString: string) => {
-  return format(localDate, formatString);
-};
-
-const parseDate = (dateString: string, formatString: string) => {
-  const localDate = parse(dateString, formatString, new Date());
-  return DateUtils.isDate(localDate) ? localDate : undefined;
-};
 
 export const DatePicker = ({
   selectedDate,
   setSelectedDate,
   disabledDays,
 }: DatePickerProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => document.removeEventListener("mousedown", handleOutsideClick);
+  });
+
+  const formatString = "dd MMM yyyy";
+
+  const handleDaySelect = (date: Date | undefined) => {
+    if (date) {
+      setSelectedDate(date);
+    }
+    setIsOpen(false);
+  };
+
+  const handleOutsideClick = (event: Event) => {
+    const element = event.target;
+    const node = element instanceof Element ? findDOMNode(element) : undefined;
+    if (node && !containerRef.current?.contains(node)) {
+      setIsOpen(false);
+    }
+  };
+
   return (
     <div className="field">
       <label className="label" htmlFor="date">
         Date
       </label>
-      <div className="control has-icons-left">
-        <DayPickerInput
-          value={selectedDate}
-          formatDate={formatDate}
-          format="dd MMM yyyy"
-          parseDate={parseDate}
-          placeholder="Select date"
-          inputProps={{
-            id: "date",
-            name: "date",
-            className: "input",
-            readOnly: true,
-          }}
-          dayPickerProps={{
-            selectedDays: selectedDate,
-            disabledDays: disabledDays,
-          }}
-          onDayChange={setSelectedDate}
+      <div className="control has-icons-left is-inline-block">
+        <input
+          id="date"
+          name="date"
+          readOnly
+          value={format(selectedDate, formatString)}
+          className="input"
+          onClick={() => setIsOpen(true)}
         />
+        {isOpen && (
+          <div className="rdp-overlay_wrapper" ref={containerRef}>
+            <div className="rdp-overlay">
+              <DayPicker
+                mode="single"
+                defaultMonth={selectedDate}
+                selected={selectedDate}
+                onSelect={handleDaySelect}
+                disabled={disabledDays}
+              />
+            </div>
+          </div>
+        )}
         <span className="icon is-left">
           <FontAwesomeIcon icon={faCalendarAlt} title="Calendar icon" />
         </span>
